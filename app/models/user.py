22050a55e7cceb153f7359b3142be6a225b1bad7
ankii22 <ankitapawar844@gmail.com>
2026-07-08 20:11:02 +0530

    feat: user table model

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy.sql import func
+
+from app.core.connection import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=True)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    username = Column(String(100), unique=True, nullable=False, index=True)
+    password_hash = Column(String(255), nullable=False)
+
+    is_active = Column(Boolean, default=True, nullable=False)
